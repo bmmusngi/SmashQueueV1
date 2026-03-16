cb643ba824707309b2c49e86673076bdd73b5fb0
bmmusngi <benedict.musngi@gmail.com>
2026-03-16 22:37:41 +0800

    bugfixing still

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Allows accessing the dev server from other devices on the network
-    port: 5173
-  }
+  base: '/', 
 })
