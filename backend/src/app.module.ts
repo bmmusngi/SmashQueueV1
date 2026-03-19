85cfcda222c0959d8fa9db1c33e2d689a708e03b
shiroben <benedict.musngi@gmail.com>
2026-03-19 15:08:15 +0800

    refactored report typescripts to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,16 @@ import { PlayerModule } from './player/player.module';
 import { QueueModule } from './queue/queue.module';
 import { SessionModule } from './session/session.module';
 import { GameModule } from './game/game.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
   imports: [
     PlayerModule, 
     QueueModule, 
     SessionModule,
-    GameModule
-  ],
+    GameModule,
+    ReportModule
+    ],
   controllers: [],
   providers: [],
 })
